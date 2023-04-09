cb77fc7603fb9f454a4fb5237bec76c6c5e57bbd
krzkro4122 <krolkrzysztof1999@gmail.com>
2023-04-09 19:52:39 +0200

    Implemented categories, products and allat and message handling, so receiving and sending.

--- kotlin-ktor-discord/src/main/kotlin/io/krzkro4122/Application.kt
+++ kotlin-ktor-discord/src/main/kotlin/io/krzkro4122/Application.kt
@@ -13,48 +13,16 @@ import io.ktor.serialization.kotlinx.*
 import kotlinx.serialization.json.*
 import kotlinx.serialization.Serializable
 import com.google.gson.annotations.SerializedName
+import io.ktor.client.request.*
 import io.ktor.util.reflect.*
-import org.json.JSONObject
-import kotlin.reflect.typeOf
 
-//import io.ktor.client.request.*
-//import io.ktor.client.statement.*
-//import io.ktor.server.application.*
-//import kotlinx.serialization.Serializable
-//import io.ktor.serialization.kotlinx.json.*
-//import io.ktor.client.plugins.contentnegotiation.*
-//import io.ktor.server.application.*
-//import io.ktor.server.routing.*
-
-//@Serializable
-//data class DiscordMessage(val content: String)
-//
-//@OptIn(InternalAPI::class)
-//suspend fun sendMessageToDiscord(channelId: String, token: String) {
-//
-//
-//    while (true) {
-//        val message = readlnOrNull() ?: ""
-//        if (message.equals("exit", true)) {
-//            httpClient.close()
-//            return
-//        }
-//
-//        try {
-//            val discordMessage = DiscordMessage(message)
-//            httpClient.request("https://discord.com/api/v10/channels/$channelId/messages") {
-//                method = HttpMethod.Post
-//                header("Authorization", "Bot $token")
-//                contentType(ContentType.Application.Json)
-//                setBody(discordMessage)
-//            }
-//        } catch (e: Exception) {
-//            println("Error while sending: " + e.localizedMessage)
-//            httpClient.close()
-//            return
-//        }
-//    }
-//}
+import io.ktor.client.statement.*
+import io.ktor.server.application.*
+import io.ktor.serialization.kotlinx.json.*
+import io.ktor.client.plugins.contentnegotiation.*
+import io.ktor.server.routing.*
+import io.ktor.util.*
+import java.lang.Exception
 
 @Serializable
 data class DiscordRequest (
@@ -78,7 +46,13 @@ data class D (
 @Serializable
 data class D_R (
     @SerializedName("properties") val properties: Properties,
-    @SerializedName("content") val content: String
+    @SerializedName("content") val content: String,
+    @SerializedName("author") val author: Author
+)
+
+@Serializable
+data class Author (
+    @SerializedName("id") val id: Long
 )
 
 @Serializable
@@ -88,8 +62,39 @@ data class Properties (
     @SerializedName("device") val device: String
 )
 
+@Serializable
+data class DiscordMessage(
+    val content: String
+)
+
+@Serializable
+data class Product (
+    val name: String,
+    val price: Int,
+    val quantity: Int,
+    val category: String
+)
 
 fun main() {
+
+    val products = arrayListOf<Product>(
+        Product("apple", 1, 100, "food"),
+        Product("banana", 2, 150, "food"),
+        Product("steering wheel", 100, 10, "car"),
+        Product("windshield wipers", 15, 50, "car"),
+        Product("Venezuela", 1000000, 1, "country"),
+        Product("Chile", 10000000, 1, "country"),
+        Product("baby wipes", 5, 200, "cleaning"),
+        Product("chlorox", 8, 70, "cleaning"),
+    )
+
+    val categories = arrayListOf<String>(
+        "food",
+        "car",
+        "country",
+        "cleaning"
+    )
+
     val dotenv = dotenv()
     val token = dotenv["DISCORD_TOKEN"]
     val channelId = dotenv["DISCORD_CHANNEL_ID"]
@@ -121,19 +126,64 @@ fun main() {
                     )
                 )
             )
-
+            val botId: Long = 1094254457958694972
             while (true) {
+
                 val incomingMessage = incoming.receive() as? Frame.Text
-//                println(otherMessage?.readText())
                 val incomingMessageContent = incomingMessage?.readText()
                 val convertedObject = Gson().fromJson(incomingMessageContent, DiscordResponse::class.java)
-                println(convertedObject.d.content)
-//                val myMessage = Scanner(System.`in`).next()
-//                if (myMessage != null) {
-//                    send(myMessage)
-//                }
+                val content = convertedObject.d.content
+
+                try {
+                    if (convertedObject.d.author.id == botId || content == null) { continue }
+                    println(content)
+
+                    var responseContent = ""
+
+                    if (content!!.contains("product")) {
+                        responseContent = "Here are the available products:\n"
+                        for (product in products) {
+                            responseContent += formatProduct(product)
+                        }
+                    } else if (content!!.contains("categor")) {
+                        for (category in categories) {
+                            if (content!!.contains(category)) {
+                                responseContent = "Here are the available products from the ${category} category:\n"
+                                for (product in products) {
+                                    if (product.category.equals(category)) {
+                                        responseContent += formatProduct(product)
+                                    }
+                                }
+                                break
+                            }
+                            responseContent = "Here are the available categories:\n"
+                            for (category in categories) {
+                                responseContent += "\t$category"
+                            }
+                        }
+                    }
+
+                    if (responseContent != "") {
+                        client.request("https://discord.com/api/v10/channels/$channelId/messages") {
+                            method = HttpMethod.Post
+                            header("Authorization", "Bot $token")
+                            contentType(ContentType.Application.Json)
+                            setBody(Gson().toJson(DiscordMessage(responseContent)))
+                        }
+                    }
+
+                } catch (e: NullPointerException) {
+                    // Please don't look here
+                } catch (e: Exception) {
+                    println("Error while sending: " + e.localizedMessage)
+                    client.close()
+                }
             }
         }
     }
     client.close()
+}
+
+fun formatProduct (product: Product): String {
+    return "\t$${product.price},\t${product.name},\t${product.category},\t(${product.quantity} left)\n"
 }
