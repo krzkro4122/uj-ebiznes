a801c2031c74162b659e86e1afd264e0dae653c6
krzkro4122 <krolkrzysztof1999@gmail.com>
2023-05-12 15:35:04 +0200

    16 e2e tests added

--- echo-go-gorm/cypress/e2e/frontend.cy.js
+++ echo-go-gorm/cypress/e2e/frontend.cy.js
@@ -6,17 +6,19 @@ const headerElements = [
   'Shopping Cart 🛒'
 ];
 
-describe('Home Page', () => {
+const productNames = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'].map((letter) => 'Product ' + letter);
+
+describe('Visits the home page', () => {
 
   beforeEach(() => {
     cy.visit(URL)
   });
 
-  it('Visits the home page', () => {
+  it('Read the home page\'s banner', () => {
     cy.contains("There's no welcome page in order to optimize user retention");
   });
 
-  it('Loads the header\'s elements', () => {
+  it('Load the header\'s elements', () => {
     headerElements.forEach((label) => {
       cy.contains(label);
     });
@@ -24,48 +26,116 @@ describe('Home Page', () => {
 
 });
 
-describe('Browse Page', () => {
+describe('Visits the browse page', () => {
 
   beforeEach(() => {
     cy.visit(URL)
     cy.contains('Browse').click()
   });
 
-  it('Visited the browse page', () => {
+  it('Visit the browse page', () => {
     cy.url().should('include', '/browse');
   });
 
-  it('Loads the header\'s elements', () => {
+  it('Load the header\'s elements', () => {
     headerElements.forEach((label) => {
       cy.contains(label);
     });
   });
 
-  it('Displays product cards', () => {
-    cy.get('Card');
+  it('Display product cards', () => {
+    productNames.forEach((product) => cy.contains(product));
   });
 
 });
 
-describe('Cart Page', () => {
+describe('Visits the (empty) cart page', () => {
 
   beforeEach(() => {
     cy.visit(URL)
     cy.contains('Shopping Cart').click()
   });
 
-  it('Visited the cart page', () => {
+  it('Visit the cart page', () => {
     cy.url().should('include', '/cart');
   });
 
-  it('Loads the header\'s elements', () => {
+  it('Load the header\'s elements', () => {
     headerElements.forEach((label) => {
       cy.contains(label);
     });
   });
 
   it('Cart is empty', () => {
-    cy.get('Card');
+    cy.contains('Your cart is empty 🚮');
   });
 
+});
+
+describe('Visits the (empty) cart page', () => {
+
+  beforeEach(() => {
+    cy.visit(URL)
+    cy.contains('Shopping Cart').click()
+  });
+
+  it('Visit the cart page', () => {
+    cy.url().should('include', '/cart');
+  });
+
+  it('Load the header\'s elements', () => {
+    headerElements.forEach((label) => {
+      cy.contains(label);
+    });
+  });
+
+  it('Cart is empty', () => {
+    cy.contains('Your cart is empty 🚮');
+  });
+
+});
+
+describe('Operates the shopping cart', () => {
+  beforeEach(() => {
+    cy.visit(URL);
+    cy.contains('Browse').click();
+    cy.contains('Buy').click();
+    cy.contains('Buy').click();
+    cy.contains('Shopping Cart').click();
+  });
+
+  it('Delete item', () => {
+    cy.contains('🗑').click();
+    cy.contains('empty');
+  });
+
+  it('Increase item\'s quantity', () => {
+    cy.contains('+').click();
+    cy.contains('Total item count: 3');
+  });
+
+  it('Decrease item\'s quantity', () => {
+    cy.contains('-').click();
+    cy.contains('Total item count: 1');
+  });
+
+  it('Buy item (not enough money)', () => {
+    cy.contains('Total item count: 2');
+    cy.window().then(($win) => {
+      cy.contains('Buy').click();
+      cy.stub($win, 'prompt').returns('0');
+      cy.stub($win, 'alert');
+    });
+    cy.contains('Total item count: 2');
+  });
+
+  it('Buy item (enough money)', () => {
+    cy.contains('Total item count: 2');
+    cy.window().then(($win) => {
+      cy.contains('Buy').click();
+      cy.stub($win, 'prompt').returns('30');
+      cy.stub($win, 'alert');
+    });
+    cy.contains('empty');
+  });
 });
