349e226ec42aacd1e8d861b8681426aa92e78cfe
krzkro4122 <krolkrzysztof1999@gmail.com>
2023-05-23 12:54:39 +0200

    Extrapolated the front-end app and it now handles fetch err somewhat...

---
+++ chatter-frontend/src/helpers/chatter.ts
@@ -0,0 +1,31 @@
+const SERVER_URL = "http://localhost:8000";
+
+interface AnswerJson {
+    answer: string
+}
+
+export async function askOpenAi(prompt: String) {
+    if (!prompt)
+        return "";
+
+    try {
+        const response = await fetch(`${SERVER_URL}${"/ask"}`, {
+            method: "POST",
+            mode: "cors",
+            cache: "no-cache",
+            credentials: "same-origin",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            redirect: "follow",
+            referrerPolicy: "no-referrer",
+            body: JSON.stringify({
+                prompt: prompt
+            }),
+        });
+        const jsonData: AnswerJson = await response.json();
+        return jsonData.answer;
+    } catch (error) {
+        return "";
+    }
+};
